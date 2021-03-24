43fcc39b95f0727c00cc62080205852a25d0ba37
douglasmg7 <douglasmg7@gmail.com>
2021-03-24 13:08:45 -0300

    config

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
+import os
 from flask import Flask
-app = Flask(__name__)
 
+app = Flask(__name__)
+#  print(os.environ['APP_SETTINGS'])
+app.config.from_object(os.environ['APP_SETTINGS'])
 
 @app.route('/')
 def hello():
-    return "Hello World!"
+    return "Hello World V2!"
 
 
 @app.route('/<name>')
